ed9a8111f60e0112ded01fbbb6770a446fc135a7
Ritik-shukla09 <9450ritik@gmail.com>
2026-03-13 22:52:50 +0530

    addd *

--- config/settings.py
+++ config/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a*y#mlr304@y*tl52e3*=%5l*lafojo*wwa8uf*_4&&xh(^y@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True    
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
