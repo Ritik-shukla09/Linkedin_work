ad6546b2317b236d2bc62151ae775a0e7ebf91d5
Ritik-shukla09 <9450ritik@gmail.com>
2026-03-13 21:27:29 +0530

    final commit

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ritik-shukla/Documents/Linkedin/env
+set -gx VIRTUAL_ENV /home/ritik-shukla/Documents/PYTHON/Linkedin/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
